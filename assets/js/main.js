5f625850f91b89f9f857e8db60012cfe8d2fd1d3
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2021-11-04 21:19:59 +0200

    basic files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,31 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
 
+/*=============MENU SHOW=======================*/
+/*Validate if constant exists*/
+if (navToggle) {
+  navToggle.addEventListener('click', function () {
+    navMenu.classList.add('show-menu');
+  });
+}
+
+/*===========MENU HIDDEN==================*/
+/*Validate if constant exists*/
+if (navClose) {
+  navClose.addEventListener('click', function () {
+    navMenu.classList.remove('show-menu');
+  });
+}
 /*=============== REMOVE MENU MOBILE ===============*/
-
+const navLink = document.querySelectorAll('.nav__link');
+function linkAction() {
+  //   const navMenu = document.getElementById('nav-menu');
+  //when we click on each nav__link, we remove the show-menu
+  navMenu.classList.remove('show-menu');
+}
+navLink.forEach((n) => n.addEventListener('click', linkAction));
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
 /*=============== QUESTIONS ACCORDION ===============*/
